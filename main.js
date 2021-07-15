c4c3eb5d6b0a60f387779457037d4a132b0a81c6
CODERBLOXx <78531418+CODERBLOXx@users.noreply.github.com>
2021-07-15 18:10:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song1 = "";
 song2 = "";
+song1_status = "";
+song2_status = "";
+scoreLeftWrist = 0;
 leftWristX = "";
 leftWristY = "";
 rightWristX = "";
@@ -26,11 +29,27 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,600,500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2 ){
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+        if(song_status == false){
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song Name = Peter Pan Song";
+        }
+    }
 }
 
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.nose.y;
         console.log("leftWristX = " + leftWristX + ", leftWristY = " + leftWristY);
